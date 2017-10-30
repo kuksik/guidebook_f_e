a945afa144d10cce932241acd8ffed7200497257
kuksik <nazarkukarkin@gmail.co>
2017-10-30 21:35:17 +0200

    added leftPanel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import TopBar from './TopBar';
+import DrawerUndockedExample from './LeftBar';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
         <TopBar />
+        <DrawerUndockedExample />
       </div>
     );
   }
---
+++ src/LeftBar/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+class DrawerUndockedExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+  
+  handleClose = () => this.setState({open: false});
+
+  render () {
+    return (
+      <MuiThemeProvider>
+        <RaisedButton
+          label="Open Drawer"
+          onClick={this.handleToggle}
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      </MuiThemeProvider>
+    );
+  }
+};
+
+export default DrawerUndockedExample;
